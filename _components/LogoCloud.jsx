36fb8590f3f2da4229d34cec282c3c2852b305ba
Chris Hartgerink <chris@libscie.org>
2024-05-30 15:22:27 +0200

    Scaffold LogoCloud

---
+++ _components/LogoCloud.jsx
@@ -0,0 +1,65 @@
+function LogoCloud() {
+    return (
+        <div className="tw-py-24 sm:tw-py-32">
+            <div className="tw-mx-auto tw-max-w-7xl tw-px-6 lg:tw-px-8">
+                <div className="-tw-mx-6 tw-grid tw-grid-cols-2 tw-gap-0.5 tw-overflow-hidden sm:tw-mx-0 sm:tw-rounded-2xl md:tw-grid-cols-3">
+                    <div className="tw-bg-white/5 tw-p-8 sm:tw-p-10">
+                    <img
+                        className="tw-max-h-12 tw-w-full tw-object-contain"
+                        src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg"
+                        // src="../public/..."
+                        alt="Transistor"
+                        width={158}
+                        height={48}
+                    />
+                    </div>
+                <div className="tw-bg-white/5 tw-p-6 sm:tw-p-10">
+                <img
+                    className="tw-max-h-12 tw-w-full tw-object-contain"
+                    src="https://tailwindui.com/img/logos/158x48/reform-logo-white.svg"
+                    alt="Reform"
+                    width={158}
+                    height={48}
+                />
+                </div>
+                <div className="tw-bg-white/5 tw-p-6 sm:tw-p-10">
+                <img
+                    className="tw-max-h-12 tw-w-full tw-object-contain"
+                    src="https://tailwindui.com/img/logos/158x48/tuple-logo-white.svg"
+                    alt="Tuple"
+                    width={158}
+                    height={48}
+                />
+                </div>
+                <div className="tw-bg-white/5 tw-p-6 sm:tw-p-10">
+                <img
+                    className="tw-max-h-12 tw-w-full tw-object-contain"
+                    src="https://tailwindui.com/img/logos/158x48/laravel-logo-white.svg"
+                    alt="Laravel"
+                    width={158}
+                    height={48}
+                />
+                </div>
+                <div className="tw-bg-white/5 tw-p-6 sm:tw-p-10">
+                <img
+                    className="tw-max-h-12 tw-w-full tw-object-contain"
+                    src="https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg"
+                    alt="SavvyCal"
+                    width={158}
+                    height={48}
+                />
+                </div>
+                <div className="tw-bg-white/5 tw-p-6 sm:tw-p-10">
+                <img
+                    className="tw-max-h-12 tw-w-full tw-object-contain"
+                    src="https://tailwindui.com/img/logos/158x48/statamic-logo-white.svg"
+                    alt="Statamic"
+                    width={158}
+                    height={48}
+                />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
