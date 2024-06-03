b9750fad4085f0ae35328a29eaa8dc48d90b02ca
Chris Hartgerink <chris@libscie.org>
2024-06-03 14:50:47 +0200

    Write out environment variables, use in PR

--- _scripts/create_release_post.R
+++ _scripts/create_release_post.R
@@ -7,6 +7,13 @@ create_release_post <- function(release_endpoint_response, pkg) {
   version <- release_endpoint_response$tag_name
   notes <- release_endpoint_response$body
 
+  if (Sys.getenv("GITHUB_ENV") != "") {
+    write(c(
+            paste0("PACKAGE=", package),
+            paste0("PACKAGE_VERSION=", version)), 
+    file = Sys.getenv("GITHUB_ENV"), append = TRUE)
+  }
+
   post_folder <- file.path("posts", paste(package, version, sep = "_"))
   dir.create(post_folder)
 
