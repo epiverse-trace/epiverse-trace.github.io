1d80de068f49682c32b9ff773878d67bfb896f12
Chris Hartgerink <chris@libscie.org>
2025-04-15 16:17:30 +0200

    Update release procedure to handle library loads

--- _scripts/create_release_post.R
+++ _scripts/create_release_post.R
@@ -10,12 +10,15 @@ create_release_post <- function(release_endpoint_response, pkg) {
   if (Sys.getenv("GITHUB_ENV") != "") {
     write(c(
             paste0("PACKAGE=", package),
-            paste0("PACKAGE_VERSION=", version)), 
+            paste0("PACKAGE_VERSION=", version)),
     file = Sys.getenv("GITHUB_ENV"), append = TRUE)
   }
 
   post_folder <- file.path("posts", paste(package, version, sep = "_"))
   dir.create(post_folder)
+
+  install.packages(package)
+  renv::snapshot(prompt = FALSE)
 
   knitr::knit_expand(
     file.path("_templates", "release.qmd"),
